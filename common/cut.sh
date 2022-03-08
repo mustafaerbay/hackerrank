ac3417539d79047c8ef55abbe2581a96d2c89e34
Mustafa Erbay <mustafaerbay365@gmail.com>
2022-03-08 11:24:20 +0300

    Create cut.sh

---
+++ common/cut.sh
@@ -0,0 +1,17 @@
+###
+https://www.hackerrank.com/challenges/text-processing-cut-4/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+###
+
+# Sample Input
+
+# Hello
+# World
+# how are you
+
+# Sample Output
+
+# Hell
+# Worl
+# how 
+
+cut -c-4
