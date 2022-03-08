0bbca5626c81705b4e462d2bf43a36201d7b4ded
Mustafa Erbay <mustafaerbay365@gmail.com>
2022-03-08 11:31:29 +0300

    Update cut.sh
    cut first 3 column

--- common/cut.sh
+++ common/cut.sh
@@ -15,3 +15,20 @@ https://www.hackerrank.com/challenges/text-processing-cut-4/problem?isFullScreen
 # how 
 
 cut -c-4
+
+#######################################################
+###
+https://www.hackerrank.com/challenges/text-processing-cut-5/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+###
+
+Cut first 3 column 
+
+# Sample input 
+A B C D
+A B C D
+
+# sample output
+A B C
+A B C
+
+cut -f -3
